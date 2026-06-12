f1ab01dc3875654bd503ed31f70778a52e9c0742
nwv4m9jdzh <nwv4m9jdzh@privaterelay.appleid.com>
2026-06-11 22:57:13 -0700

    doplneny pisnicky prvni

--- pisne.js
+++ pisne.js
@@ -12,13 +12,15 @@ var PISNE_TEXT = `
 ## Dobrý Mravenci ##
 && Tomáš Klus &&
 
-[H Em H Em]
+[Intro: H Em]
+
+[Verse: H Em H Em]
 Pro jednoho je i moc málo
 Druhý má zase všeho dost
 Na třetího se nedostalo
 Čtvrtého trápí minulost
  
-[H Em H Em]
+[Verse: H Em H Em]
 Pátý v patové situaci
 Jako sám velký Achilles
 Šestý má smysl pro legraci
@@ -32,23 +34,23 @@ A že má své bolesti
 Až dozví se, že patrně
 Se dostal na scestí
  
-[ D G ]
+[D G]
 la la lalalaj lalalaj lalalaj lalalalalaláááj
 la la lalalaj lalalaj lalalaj lalalalalaláááj
  
-[H Em H Em]
+[Verse: H Em H Em]
 Křížová cesta s zrnkem rýže
 Ve jménu královny matky
 Zatížen s tíhou zemské tíže
 Však myslí jen na hmotné statky
  
-[H Em H Em]
+[Verse: H Em H Em]
 V mnohém si společně libují
 Malí lidé, velcí mravenci
 Poslušně vůdce následují
 Kradou cukr v cizí kredenci
  
-[chorus: C G C G ]
+[Chorus: C G C G ]
 Neb komu zakrní mozeček
 Dělá jako pitomeček
 Co jiný přikáže
@@ -56,23 +58,23 @@ A za hrst korálků, morálku
 Pokřiví si obálku
 Do níž byl zatažen
  
-[ D G ]
+[D G]
 la la lalalaj lalalaj lalalaj lalalalalaláááj
 la la lalalaj lalalaj lalalaj lalalalalaláááj
 
-[H Em H Em]
+[Verse: H Em H Em]
 Kdo jednou přijme výzvu k boji
 Je předem k smrti odsouzen
 Jako mravenec v lese neobstojí
 Tak člověk neovládne zem
  
-[H Em H Em]
+[Verse: H Em H Em]
 A až se v našem zorném poli
 Rozpohybují póly
 Bude už pozdě na cokoli
 Pozdě na cokoli
  
-[chorus: C G C G ]
+[Chorus: C G C G ]
 Syn křičí na matku
 To není v pořádku
 Mít u zadku své předky rodové
@@ -80,18 +82,387 @@ Vezměte rozum do hrsti
 Konec nesmíte dopustit
 Vy hlavy skopové
  
-[ D G ]
+[D G]
 la la lalalaj lalalaj lalalaj lalalalalaláááj
 la la lalalaj lalalaj lalalaj lalalalalaláááj
 la la lalalaj lalalaj lalalaj lalalalalaláááj
 la la lalalaj lalalaj lalalaj lalalalalaláááj
 
 
-## Nebe plný hvězd ##
-&& ukázková píseň &&
+## Pocity ##
+&& Tomáš Klus &&
 
-[sloka: Am F C G]
-Sedíme u ohně, plamen táhne výš,
+[G D Em C]
+Z posledních pocitů
+poskládám ještě jednou úžasnou chvíli
+Je to tím, že jsi tu,
+možná tím, že kdysi jsme byli Ty a já,
+my dva, dvě nahý těla,
+Tak neříkej, že jinak jsi to chtěla,
+tak neříkej
+Neříkej, neříkej mi nic
+
+Stala ses do noci
+z ničeho nic moje platonická láska
+unaven, bezmocí,
+usínám vedle tebe, něco ve mně praská
+A ranní probuzení
+a slova o štěstí, neboj se to nic není
+Pohled na okouzlení,
+a prázdný náměstí na znamení,
+
+Jenže ty neslyšíš,
+jenže ty neposloucháš,
+snad ani nevidíš,
+nebo spíš nechceš vidět
+a druhejm závidíš
+a v očích kapky slaný vody
+zkus změnu, uvidíš
+a vítej do svobody
+
+Jsi anděl, netušíš,
+Anděl co ze strachu mu utrhli křídla
+A až to ucítíš, zkus kašlat na pravidla
+Říkej si vo mně co chceš,
+já jsem byl vodjakživa blázen
+Nevim co nechápeš,
+ale vrať se na zem
+
+Jenže ty neslyšíš,
+jenže ty neposloucháš,
+snad ani nevidíš,
+nebo spíš nechceš vidět
+a druhejm závidíš
+a v očích kapky slaný vody
+zkus změnu, uvidíš
+a vítej do svobody
+
+Z posledních pocitů
+poskládám ještě jednou úžasnou chvíli
+Je to tím, že jsi tu,
+možná tím, že kdysi jsme byli Ty a já,
+my dva, dvě nahý těla,
+Tak neříkej, že jinak jsi to chtěla,
+tak neříkej
+Neříkej, neříkej mi nic
+
+
+
+## Nina ##
+&& Tomáš Klus &&
+
+[D Hm G A]
+Dnes v noci jsem ze spaní křičela tvoje jméno,
+já vím, že nejsi rád, ale nejde zapomenout,
+jak při každém slově přivíráš víčka,
+prosím vyslyš moji zpověď, už jsme starý na psaníčka.
+
+Jsem bytost z vodních par, živa jenom z tvého dechu,
+já vím, že nejsi rád, a že je ti to k vzteku,
+chci ti všechno říct a pak se někam schovat,
+třeba pochopíš, jak je těžké nemilovat.
+
+Zase zrychlil se mi dech, jak maratónským běžcům,
+co je to za příběh, bez lásky, bez milenců,
+nemáš slov, patrně všechna patří jiným,
+prosím proměň je s nimi, ve sny, v gesta i činy.
+
+A já tu zůstanu, ztracené malířské plátno,
+třeba se vrátíš a já zas nechám se napnout,
+prosím maluj mě, tvoř, k obrazu svému,
+nech mě shořet, už nikdy o nás nemluv.
+
+Jsi mé úzko, jsi krev z řezných ran,
+jsi ten kdo vchází nepozván,
+jsi zvuk, když padnou mi na rety slzy múz.
+
+Jsi mé úzko, jsi krev z řezných ran,
+ačkoliv nechci, jsi ve mě uschován,
+jsi zvuk, když padnou mi na rety slzy múz.
+
+Až splynu se vzduchem, nechám rozplakat nebe,
+budu vším, tím, co lidi k propasti svede,
+budu Krysařovou flétnou a ozvěna v tvé duši,
+pak ptáci tiše vzlétnou, by nedali tušit.
+
+Že se nebe nakloní a zapře se světem,
+tvé černé svědomí poprvé promluví k obětem,
+nerovných bojů tvé sebestředné války,
+srdečních nepokojů cos pozoroval z dálky.
+
+Jsi mé úzko, jsi krev z řezných ran,
+ačkoliv nechci, jsi ve mě uschován,
+jsi zvuk, když padnou mi na rety slzy múz.
+
+Jsi mé úzko, jsi krev z řezných ran,
+jsi ten kdo vchází nepozván,
+jsi zvuk, když padnou mi na rety slzy múz.
+
+Jsi mé úzko, jsi krev z řezných ran,
+ačkoliv nechci, jsi ve mě uschován,
+jsi zvuk, když padnou mi na rety slzy múz.
+
+Jsi mé úzko, jsi krev z řezných ran,
+jsi ten kdo vchází nepozván,
+jsi zvuk, když padnou mi na rety slzy múz.
+
+
+
+
+## With Or Without You ##
+&& U2 &&
+
+[D A Hm G]
+See the stone set in your eyes
+See the thorn twist in your side
+I'll wait for you
+Sleight of hand and twist of fate
+On a bed of nails she makes me wait
+And I wait without you
+
+With or without you
+With or without you
+
+Through the storm, we reach the shore
+You give it all but I want more
+And I'm waiting for you
+
+With or without you
+With or without you, ah-ah
+I can't live with or without you
+
+And you give yourself away
+And you give yourself away
+And you give, and you give
+And you give yourself away
+
+My hands are tied, my body bruised
+She got me with nothing to win
+And nothing left to lose
+
+And you give yourself away
+And you give yourself away
+And you give, and you give
+And you give yourself away
+
+With or without you
+With or without you, ohh
+I can't live with or without you
+
+With or without you
+With or without you, ohh
+I can't live with or without you
+With or without you
+
+
+## Cesta ##
+&& Kryštof &&
+
+[C G Ami F G]
+
+Tou cestou
+Tím směrem prý bych se dávno měl dát
+Když sněží, jde to stěží, ale sněhy pak tají
+Kus něhy ti za nehty slíbí a dají
+Víc síly
+Se prát
+Na dně víc dávat
+Než brát.
+
+A i když se vleče a je schůdná jen v kleče
+Donutí přestat se zbytečně ptát
+Jestli se blížím k cíli
+Kolik zbývá víry
+Kam zvou
+Svodidla, co po tmě mi lžou?
+
+Snad couvám zpátky
+A plýtvám řádky
+Co řvou
+Že už mi doma neotevřou
+
+Nebo jít s proudem
+Na lusknutí prstu se začít hned smát,
+Mít svůj chodník slávy a před sebou davy
+A přes zkroucená záda být součástí stáda
+
+Ale zpívat
+A hrát
+Kotníky líbat
+A stát
+Na křídlech všech slavíků
+A vlastně už ze zvyku
+Přestat se zbytečně ptát
+
+Jestli se blížím k cíli
+Kolik zbývá víry
+Kam zvou
+Svodidla, co po tmě mi lžou?
+
+Snad couvám zpátky
+A plýtvám řádky
+Co řvou
+Že už mi doma neotevřou
+
+Jestli se blížím k cíli
+Kolik zbývá víry
+Kam zvou
+Svodidla, co po tmě mi lžou?
+
+Snad couvám zpátky
+A plýtvám řádky
+Co řvou
+Že už mi doma neotevřou
+
+
+## Kometa ##
+&& Jaromír Nohavica &&
+
+[Verse: Ami Dmi G7 C E7]
+Spatřil jsem kometu, oblohou letěla,
+chtěl jsem jí zazpívat, ona mi zmizela,
+zmizela jako laň u lesa v remízku,
+v očích mi zbylo jen pár žlutých penízků.
+
+Penízky ukryl jsem do hlíny pod dubem,
+až příště přiletí, my už tu nebudem,
+my už tu nebudem, ach, pýcho marnivá,
+spatřil jsem kometu, chtěl jsem jí zazpívat.
+
+[Refrain: Ami Dmi G7 C E E7]
+O vodě, o trávě, o lese,
+o smrti, se kterou smířit nejde se,
+o lásce, o zradě, o světě
+a o všech lidech, co kdy žili na téhle planetě.
+
+[Verse: Ami Dmi G7 C E7]
+Na hvězdném nádraží cinkají vagóny,
+pan Kepler rozepsal nebeské zákony,
+hledal, až nalezl v hvězdářských triedrechtajemství, která teď neseme na bedrech.
+
+Velká a odvěká tajemství přírody,
+že jenom z člověka člověk se narodí,
+že kořen s větvemi ve strom se spojují
+a krev našich nadějí vesmírem putuje.
+
+[Refrain: Ami Dmi G7 C E E7]
+Na na na ...
+
+[Verse: Ami Dmi G7 C E7]
+Spatřil jsem kometu, byla jak reliéf
+zpod rukou umělce, který už nežije,
+šplhal jsem do nebe, chtěl jsem ji osahat,
+marnost mne vysvlékla celého donaha.
+
+Jak socha Davida z bílého mramoru
+stál jsem a hleděl jsem, hleděl jsem nahoru,
+až příště přiletí, ach, pýcho marnivá,
+my už tu nebudem, ale jiný jí zazpívá.
+
+[Refrain: Ami Dmi G7 C E E7]
+O vodě, o trávě, o lese,
+o smrti, se kterou smířit nejde se,
+o lásce, o zradě, o světě,
+bude to písnička o nás a kometě ...
+
+
+## Kde si včera bol ##
+&& Romstar Vyškov &&
+
+[F# B D#mi G#mi C#]
+Kde si včera bol
+Si zase ožratý
+Si totálne na mol, to mi teraz vysvetli
+Kde si včera bol, si zase ožratý
+Si totálne na mol, to mi teraz vysvetli
+
+Láska mám ťa rád, ja ťa milujem,
+Ja si ťa zoberem
+Teraz mi neveríš, lebo mám vypité
+Ja si ťa zoberem
+
+Kde si včera bol, si zase ožratý
+Si totálne na mol, to mi teraz vysvetli
+Kde si včera bol, si zase ožratý
+Si totálne na mol, to mi teraz vysvetli
+
+Láska mám ťa rád, ja ťa milujem,
+Ja si ťa zoberem
+Teraz mi neveríš, lebo mám vypité
+Ja si ťa zoberem
+
+Láska mám ťa rád, ja ťa milujem,
+Ja si ťa zoberem
+Teraz mi neveríš, lebo mám vypité
+Ja si ťa zoberem
+
+Kde si včera bol, si zase ožratý
+Si totálne na mol, to mi teraz vysvetli
+Kde si včera bol, si zase ožratý
+Si totálne na mol, to mi teraz vysvetli
+
+Láska mám ťa rád, ja ťa milujem,
+Ja si ťa zoberem
+Teraz mi neveríš, lebo mám vypité
+Ja si ťa zoberem
+
+Láska mám ťa rád, ja ťa milujem,
+Ja si ťa zoberem
+Teraz mi neveríš, lebo mám vypité
+Ja si ťa zoberem
+
+Láska mám ťa rád, ja ťa milujem,
+Ja si ťa zoberem
+Teraz mi neveríš, lebo mám vypité
+Ja si ťa zoberem
+
+
+
+## Perfect ##
+&& Ed Sheeran &&
+
+[C Ami F G]
+I found a love for me
+Darling, just dive right in and follow my lead
+Well, I found a girl, beautiful and sweet
+Oh, I never knew you were the someone waiting for me
+'Cause we were just kids when we fell in love
+Not knowing what it was
+I will not give you up this time
+But darling, just kiss me slow
+Your heart is all I own
+And in your eyes you're holding mine
+
+Baby, I'm dancing in the dark
+With you between my arms
+Barefoot on the grass
+Listening to our favourite song
+When you said you looked a mess
+I whispered underneath my breath
+But you heard it,
+Darling, you look perfect tonight
+
+Well, I found a woman, stronger than anyone I know
+She shares my dreams, I hope that someday I'll share her home
+I found a love to carry more than just my secrets
+To carry love, to carry children of our own
+We are still kids but we're so in love
+Fighting against all odds
+I know we'll be alright this time
+Darling, just hold my hand
+Be my girl, I'll be your man
+I see my future in your eyes
+
+Baby, I'm dancing in the dark
+With you between my arms
+Barefoot on the grass
+Listening to our favourite song
+I have faith in what I see
+Now I know I have met an angel
+In person and she looks perfect
+No I don't deserve this,
+You look perfect tonight
+
 
 
 `;
